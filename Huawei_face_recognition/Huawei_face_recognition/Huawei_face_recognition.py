6b358d6f145105a4f6a62a60f5d11fe7a0b2e71d
agui1era <oscar@aguilera.xyz>
2019-10-11 18:31:19 -0300

    Update Huawei_face_recognition.py

--- Huawei_face_recognition/Huawei_face_recognition/Huawei_face_recognition.py
+++ Huawei_face_recognition/Huawei_face_recognition/Huawei_face_recognition.py
@@ -1,21 +1,40 @@
 
 import requests
 import base64
+import json
+
 image_base="C:/base_image.jpg"
+sample_image="C:/sample_image.jpg"
 
 with open(image_base, "rb") as img_file:
     my_string = base64.b64encode(img_file.read())
 print(my_string)
-# importing the requests library 
-import requests 
 
-# defining the api-endpoint 
 API_ENDPOINT = "https://iam.ap-southeast-1.myhuaweicloud.com/v3/auth/tokens"
 
 
 
-data = {'api_option':'paste', 
-		'api_paste_format':'python'} 
+data = {
+  "auth": {
+    "identity": {
+      "methods": ["password"],
+      "password": {
+        "user": {
+          "name": "oaguilera", 
+           "password":"",
+          "domain": {
+            "name": "oaguilera" 
+          }
+        }
+      }
+    },
+    "scope": {
+      "project": {
+        "name": "ap-southeast-1" 
+      }
+    }
+  }
+}
 
 # sending post request and saving response as response object 
 r = requests.post(url = API_ENDPOINT, data = data) 
